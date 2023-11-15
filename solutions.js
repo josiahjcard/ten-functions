7cfa01b857df0fcc1baa46ebb653f48bc5ae8fe1
Josiah Cardenas <josiahjcard@gmail.com>
2023-11-15 15:50:42 -0600

    added Functions Exercises

--- solutions.js
+++ solutions.js
@@ -21,3 +21,117 @@
  * concat
  */
 
+//Question 1
+function isTrue(userInput) {
+    if( userInput === true) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+//Question 2
+function isFalse(userInput) {
+    if( userInput === false) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+//Question 3
+function not(userInput) {
+    if (!(userInput)) {
+        return true;
+    } else{
+        return false;
+    }
+}
+
+//Question 4
+function addOne(userInput) {
+
+       return Number(userInput) + 1;
+}
+
+
+//Question 5
+function isEven(input) {
+    if(typeof input === "number" || typeof input === "string" && !isNaN(input)) {
+        return Number(input) % 2 === 0;
+    } else {
+        return false;
+    }
+}
+
+
+//Question 6
+function isIdentical(input, number) {
+    if( input === number) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//Qusetion 7
+
+function isEqual(input, number){
+    if( input == number) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Question 8
+function or(input, number) {
+    return (input || number);
+}
+
+//Question 9
+
+function and(input, number) {
+    return input && number;
+}
+
+
+
+//question 10
+function concat(input, number) {
+    if(typeof input === "string" && typeof number === "string") {
+        return input + number;
+    }
+
+    if(typeof input === "number" && typeof number === "number") {
+        return input.toString() + number.toString();
+    }
+
+    if(typeof input === "string" && typeof number === "number") {
+        return input + number.toString();
+    }
+
+    if(typeof input === "boolean" && typeof number === "boolean") {
+        return input.toString() + number.toString();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
